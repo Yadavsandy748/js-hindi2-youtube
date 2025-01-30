5024d7d0908073317ef1f50a676f62b51b5eb415
Yadavsandy748 <112010100+Yadavsandy748@users.noreply.github.com>
2025-01-30 10:59:38 +0000

    convertion of operation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello") // we are using node.js not browser;
+
+// number => 2 to the power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol 
+
+// non-primative
+// object  
+
+let state = null
+console.log(typeof "sandeep")
+console.log(typeof state)
+
---
+++ 01_basics/03_convoperation.js
@@ -0,0 +1,20 @@
+let score ="sandeep"
+
+//console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN
+// true =>1; false => 0 
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let isboolean = ""
+console.log(isboolean)
